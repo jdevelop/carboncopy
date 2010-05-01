2497913a31d7bca5f6d1e2c71e53dc7b433edc7c
Eugene <bofh@redwerk.com>
2010-05-02 01:49:32 +0300

    Rearranged code, make it possible to create root chain

--- CarbonCopy/StorageInit.hs
+++ CarbonCopy/StorageInit.hs
@@ -1,19 +1,13 @@
 module CarbonCopy.StorageInit (
-    storageInit,
-    matchFromHeader
+    storageInit
     ) where
 
-import Prelude as P
-import Control.Monad
 import Control.Monad.IfElse
 import Data.Maybe
-import Data.List as L
 import CarbonCopy.EmailStorage
 import CarbonCopy.MailHeaders
 import CarbonCopy.HeadersStorage
-import CarbonCopy.ThreadBuilder as TB
-import Text.ParserCombinators.ReadP as R
-import Data.ByteString.Char8 as BStr
+import CarbonCopy.ThreadBuilder
 
 storageInit :: String -> FilePath -> Storage StrHeader -> IO ()
 storageInit email rootDir storage = visitEmailsRecursively rootDir ( processHeader email storage )
@@ -26,43 +20,7 @@ processHeader email storage content = processHeader' chain
         processHeader' (Just myChain) = processHeader'' hdrsMatchFound
             where
                 fromMsgId = current myChain
+                inReplyTo = previous myChain
                 processHeader'' True = unlessM (storage `hdrExists` fromMsgId) $ storage `hdrAdd` fromMsgId
-                processHeader'' False = return ()
+                processHeader'' False = whenM (storage `hdrExists` inReplyTo) $ storage `hdrAdd` fromMsgId
         processHeader' _ = return ()
-            
-
-matchFromHeader :: ByteString -> String -> ( Maybe MsgidChain, Bool )
-matchFromHeader content email = ( chain, hdrsMatchFound )
-    where 
-        headers = visitHeader content
-        hdrsMatchFound = or $ P.map hdrMatch headers
-        chain = prepareChain headers
-        hdrMatch (Header name value) = name == from_hdr && email `L.isInfixOf` value
-
-
-findHeaderByName :: String -> [StrHeader] -> Maybe (StrHeader)
-findHeaderByName hdrName = L.find ( (hdrName ==) . name )
-
-prepareChain :: [StrHeader] -> Maybe (MsgidChain)
-prepareChain hdrs = do
-                    msgIdHdr <- findHeaderByName msg_id_hdr hdrs
-                    inReplyToHdr <- findHeaderByName in_reply_to_hdr hdrs
-                    Just (Chain msgIdHdr inReplyToHdr)
-
-
-visitHeader :: ByteString -> [StrHeader]
-visitHeader = flip extractHeaders ( 
-                                    headerValue from_hdr +++ 
-                                    TB.headerVal msg_id_hdr +++ 
-                                    TB.headerVal in_reply_to_hdr 
-                                  )
-
-headerValue :: String -> ReadP StrHeader
-headerValue hdrName = do
-            R.string hdrName
-            R.skipSpaces
-            R.char ':'
-            R.skipSpaces
-            xs <- R.many R.get
-            R.satisfy ( == '\n' )
-            return (Header hdrName xs)
--- CarbonCopy/ThreadBuilder.hs
+++ CarbonCopy/ThreadBuilder.hs
@@ -3,7 +3,8 @@ module CarbonCopy.ThreadBuilder (
     headerVal,
     saveMatchingChain,
     Chain(..),
-    getChain,
+    prepareChain,
+    matchFromHeader,
     MsgidChain) where
 
 import Control.Monad
@@ -12,10 +13,61 @@ import CarbonCopy.HeadersStorage
 import Data.Maybe
 import Data.ByteString.Char8 as BStr
 import Text.ParserCombinators.ReadP as R
+import Data.List as L
+import Prelude as P
 
-data Chain keyT valueT = Chain { current, previous :: Header keyT valueT}
+data Chain keyT valueT = Chain { current, previous :: Header keyT valueT} | Root { current :: Header keyT valueT }
 
 type MsgidChain = Chain String String
+
+
+saveMatchingChain :: Storage (Header keyT valueT) -> Chain keyT valueT -> IO (Bool)
+saveMatchingChain storage 
+                  ( Chain { current=myCurrent, previous=myPrevious } ) = do
+    prevHdrExists <- storage `hdrExists` myPrevious
+    currHdrExists <- storage `hdrExists` myCurrent
+    when (prevHdrExists && not currHdrExists) $ storage `hdrAdd` myCurrent
+    return ( prevHdrExists || currHdrExists )
+
+findHeaderByName :: String -> [StrHeader] -> Maybe (StrHeader)
+findHeaderByName hdrName = L.find ( (hdrName ==) . name )
+
+prepareChain :: [StrHeader] -> Maybe (MsgidChain)
+prepareChain hdrs = processNextHeader $ findHeaderByName in_reply_to_hdr hdrs
+    where 
+        processNextHeader (Just inReplyToHdr) = do
+            msgIdHdr <- findHeaderByName msg_id_hdr hdrs
+            Just (Chain msgIdHdr inReplyToHdr)
+        processNextHeader Nothing = do
+            msgIdHdr <- findHeaderByName msg_id_hdr hdrs
+            Just (Root msgIdHdr)
+            
+            
+
+matchFromHeader :: ByteString -> String -> ( Maybe MsgidChain, Bool )
+matchFromHeader content email = ( chain, hdrsMatchFound )
+    where 
+        headers = visitHeader content
+        hdrsMatchFound = or $ P.map hdrMatch headers
+        chain = prepareChain headers
+        hdrMatch (Header name value) = name == from_hdr && email `L.isInfixOf` value
+
+visitHeader :: ByteString -> [StrHeader]
+visitHeader = flip extractHeaders ( 
+                                    headerValue from_hdr +++ 
+                                    headerVal msg_id_hdr +++ 
+                                    headerVal in_reply_to_hdr 
+                                  )
+
+headerValue :: String -> ReadP StrHeader
+headerValue hdrName = do
+            R.string hdrName
+            R.skipSpaces
+            R.char ':'
+            R.skipSpaces
+            xs <- R.many R.get
+            R.satisfy ( == '\n' )
+            return (Header hdrName xs)
 
 headerVal :: String -> ReadP StrHeader
 headerVal name = do 
@@ -27,23 +79,3 @@ headerVal name = do
             xs <- R.many R.get
             R.char '>'
             return (Header name xs)
-
-
-getChain :: ByteString -> Maybe MsgidChain
-getChain content = getChain' headers
-    where 
-        headers = extractHeaders content ( (headerVal msg_id_hdr) +++ (headerVal in_reply_to_hdr) )
-        getChain' (h1:h2:[]) =Just chain
-            where
-                chain | name h1 == msg_id_hdr = Chain {current = h1, previous = h2 }
-                      | otherwise = Chain {current = h2, previous = h1 }
-        getChain' _          = Nothing
-
-
-saveMatchingChain :: Storage (Header keyT valueT) -> Chain keyT valueT -> IO (Bool)
-saveMatchingChain storage 
-                  ( Chain { current=myCurrent, previous=myPrevious } ) = do
-    prevHdrExists <- storage `hdrExists` myPrevious
-    currHdrExists <- storage `hdrExists` myCurrent
-    when (prevHdrExists && not currHdrExists) $ storage `hdrAdd` myCurrent
-    return ( prevHdrExists || currHdrExists )
